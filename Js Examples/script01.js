613c17a8ee152257ef40fc1c3a7b575f6fe6e771
UmashaJayawardhana <unethmi0@gmail.com>
2024-03-21 00:11:58 +0530

    add files

---
+++ Js Examples/script01.js
@@ -0,0 +1,6 @@
+document.write("Hello JavaScript");
+document.write("<br/>");
+document.write("Js concat operators are '+' and ',' ")
+document.write("<br/>");
+document.write("I want it that way" , "<br/>" , "<h1>I'm on my way</h1>");
+document.write("Every nigths in my derams" + "<br/>" + "I see you I feel you");
